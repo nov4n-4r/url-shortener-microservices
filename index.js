28d54736ba3bd6c0de1aadcbea49b86781822413
Novan_ <83200466+nov4n-4r@users.noreply.github.com>
2022-07-26 02:23:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser")
 const shorturlModel = require("./model/shorturl")
-
+const Model = new shorturlModel()
 const app = express();
 
 
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,40 +19,31 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.route(`shorturl`)
-    .get((req, res) => {
-        res.render("shorturl")
-    })
-    
 app.route("/api/shorturl")
-    .post((req, res) => {
-        if(!req.body.url) res.redirect(`shorturl`)
-        const pattern = /^http(s)?:\/\/(www)\.(\w)+\.(.)+$/gi
-        const match = pattern.test(req.body.url)
-        if(match){
-            const Model = new shorturlModel()
-            Model.insertUrl(req.body.url, (data) => 
-                res.json({
-                    original_url : data.url,
-                    shorturl : data.id
-                })
-            )
-        }else{
-            res.json({
-                error : "invalid url"
-            })
-        }
-    })
+  .post((req, res) => {
+    if (!req.body.url) res.redirect(`shorturl`)
+    const pattern = /^http(s)?:\/\/(.)+$/gi
+    const match = pattern.test(req.body.url)
+    if (match) {
+      const result = Model.insertUrl(req.body.url)
+//       console.log(result)
+      return res.json({
+        original_url: result.url,
+        short_url: result.id
+      })
+    } else {
+      res.json({
+        error: "invalid url"
+      })
+    }
+  })
 
-app.route(`/api/shorturl/:id`)
-    .get((req, res) => {
-        const Model = new shorturlModel()
-        Model.getUrl( {id : parseInt(req.params.id)})
-            .then(data => data[0])
-            .then(data => res.redirect(data.url))
-            .catch(err => res.json({error : "Shorturl not found"}))
-    })
+app.route("/api/shorturl/:id")
+  .get((req, res) => {
+//     console.log(req.params)
+    const result = Model.getUrlById(parseInt(req.params.id))
+    res.redirect(result.url)
+  })
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
