cf8485cfc4ffda4144baba7f5ff7bce14af9bb04
Novan_ <83200466+nov4n-4r@users.noreply.github.com>
2022-07-26 02:24:36 -0400

    Update shorturl.js

--- model/shorturl.js
+++ model/shorturl.js
@@ -1,66 +1,47 @@
-const mongodb = require("mongodb")
-class urlShortenerMicroserviesModel{
-    constructor(url = "mongodb+srv://novan:12345@cluster0.vrmp5.mongodb.net/?retryWrites=true&w=majority"){
-        this.client = new mongodb.MongoClient(url)
-        this.db = this.client.db("fcc-challenge")
-        this.insertUrl = this.insertUrl.bind(this)
-        this.collection = this.db.collection("url-shortener-microservices")
-    }
+class Model {
+  constructor() {
+    this.data = [
+      {
+        id: 1,
+        url: "https://www.freecodecamp.org"
+      }
+    ]
+  }
 
-    connect(){
-        this.client.connect()
-            .then( () => console.log("Connection created") )
-            .catch( err => console.log(err) )  
-    }
+  getLatestID() {
+    let sorted = this.data.sort((a, b) => b.id - a.id)
+    if (!sorted[0]) return 0
+    return sorted[0].id
+  }
 
-    insertUrl(url, callback = res => {console.log(res);}){
-        this.getUrl({url : url})
-            // Checking is the url registered or not
-            .then(data => typeof(data[0]) != "undefined")
-            .then(registered => {
-                // if not registered then register it
-                if(!registered){
-                    this.getLatestID()
-                        // Add 1 for id (auto increment) 
-                        .then(id => id+1 )
-                        // Inserting data
-                        .then(id => 
-                            this.collection.insertOne({id : id, url : url})
-                        )
-                        .then(inserted => inserted.insertedId.toString())
-                        .then(insertedID => 
-                            this.getUrl( {_id : mongodb.ObjectId(insertedID)}, data => {
-                                data = data[0]
-                                callback(data)
-                            } )
-                        )
-                // if its registered
-                }else{ 
-                    this.getUrl( {url : url}, data => {
-                        data = data[0]
-                        callback(data);
-                    } )
-                }
-            })
-        
-    }
-    
-    async getLatestID(){
-        return await this.collection.find().sort({id : -1}).limit(1).toArray()
-            .then(res => res[0])
-            .then(res => res.id)
-            .catch(id => 0)
-    }
+  getAllUrl() {
+    return this.data
+  }
 
-    async getUrl(query={}, callback){
-        if(!callback){
-            return await this.collection.find(query).toArray()
-        }else{
-            await this.collection.find(query).toArray()
-                .then(data => callback(data))
-        }
-    }
+  getUrl(url) {
+    return (this.data.filter(obj => obj.url === url))[0]
+  }
 
+  getUrlById(id) {
+    return (this.data.filter(obj => obj.id === parseInt(id)))[0]
+  }
+
+  insertUrl(url) {
+    const urlExist = this.getUrl(url)
+    if (urlExist) return urlExist
+    const obj = {
+      id: this.getLatestID() + 1,
+      url: url
+    }
+    const succes = this.data.push(obj)
+    if (succes) {
+      return obj
+    } else {
+      return {
+        error: "Something went wrong"
+      }
+    }
+  }
 }
 
-module.exports = urlShortenerMicroserviesModel
+module.exports = Model
